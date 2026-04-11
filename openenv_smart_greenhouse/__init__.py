2e6f55db2d180a72fdb0fc6477a5042c9c9d84c7
jefrinbeno <jefrinbeno35@gmail.com>
2026-04-11 05:22:30 +0000

    Submission 22: Final alignment of package structure and entry points

--- openenv_smart_greenhouse/__init__.py
+++ openenv_smart_greenhouse/__init__.py
@@ -1,15 +1,1 @@
-import openenv
-
-class GreenhouseEnv(openenv.Environment):
-    def __init__(self):
-        super().__init__()
-        self.register_task(id="temp_control", name="Temperature Control", grader=True)
-        self.register_task(id="hum_control", name="Humidity Control", grader=True)
-        self.register_task(id="res_mgmnt", name="Resource Management", grader=True)
-
-    def step(self, action):
-        # Return a reward strictly between 0 and 1
-        return {"obs": "stable", "reward": 0.95, "done": False, "info": {}}
-
-    def reset(self):
-        return {"obs": "initial"}
+from .env import GreenhouseEnv
